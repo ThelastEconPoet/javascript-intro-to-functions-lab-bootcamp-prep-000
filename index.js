bbab3806b9d59e7a0744dd2580784986f8460614
Aaron G Meininger <a.g.meininger@gmail.com>
2018-05-03 16:03:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase()) { 
-   return  string("I can't hear you")
+  if (whisper()) { 
+   return  ("I can't hear you")
 }
 }
